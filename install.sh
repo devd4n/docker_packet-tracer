d96016c232f64aca639e028918737e7c569bb270
devd4n <18461526+devd4n@users.noreply.github.com>
2023-03-14 16:03:15 +0100

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+curl https://raw.githubusercontent.com/devd4n/docker_packet-tracer/main/Makefile
+curl https://raw.githubusercontent.com/devd4n/docker_packet-tracer/main/Dockerfile
+curl https://raw.githubusercontent.com/devd4n/docker_packet-tracer/main/packettracer
