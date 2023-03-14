434cf21ac304280d19ecc5d9e48049ac0cd99c26
devd4n <18461526+devd4n@users.noreply.github.com>
2023-03-14 16:15:06 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-curl https://raw.githubusercontent.com/devd4n/docker_packet-tracer/main/Makefile -o Makefile
 curl https://raw.githubusercontent.com/devd4n/docker_packet-tracer/main/Dockerfile -o Dockerfile
 curl https://raw.githubusercontent.com/devd4n/docker_packet-tracer/main/packettracer -o packettracer
